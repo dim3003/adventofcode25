ec8fd552dc755ddd3883cd0a3c927571c2583761
dimitri <andre.d3003@gmail.com>
2025-12-04 20:47:11 +0100

    part 2

--- first/app.cs
+++ first/app.cs
@@ -2,19 +2,25 @@ using System.IO;
 
 int numberOfZeros = 0;
 int currentDialNumber = 50;
+
 foreach (var line in File.ReadLines("data.txt"))
 {
-    var numberOfDials = int.Parse(line.Substring(1));
+    int n = int.Parse(line.Substring(1));
+    int delta = line[0] == 'R' ? n : -n;
 
-    if (line[0] == 'R')
-        currentDialNumber += numberOfDials;
-    else
-        currentDialNumber -= numberOfDials;
+    int prev = currentDialNumber;
+    int newRaw = prev + delta;
 
-    currentDialNumber = ((currentDialNumber % 100) + 100) % 100;
+    int crossings = 0;
+    if(delta > 0){
+      crossings += (newRaw - (newRaw % 100)) / 100; 
+    }
+    if(newRaw <= 0){
+      crossings += (-newRaw - (-newRaw % 100)) /100 + (prev == 0 ? 0 : 1);
+    }
 
-    if (currentDialNumber == 0)
-        numberOfZeros++;
+    numberOfZeros += crossings;
+    currentDialNumber = ((newRaw % 100) + 100) % 100;
 }
 
 Console.WriteLine(numberOfZeros);
