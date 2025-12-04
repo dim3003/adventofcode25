54c5ff685f876699d9f718e361b3543759058ce8
dimitri <andre.d3003@gmail.com>
2025-12-04 21:00:40 +0100

    minimize

--- first/app.cs
+++ first/app.cs
@@ -1,26 +1,15 @@
 using System.IO;
-
 int numberOfZeros = 0;
 int currentDialNumber = 50;
-
 foreach (var line in File.ReadLines("data.txt"))
 {
     int n = int.Parse(line.Substring(1));
     int delta = line[0] == 'R' ? n : -n;
-
-    int prev = currentDialNumber;
-    int newRaw = prev + delta;
-
-    int crossings = 0;
-    if(delta > 0){
-      crossings += (newRaw - (newRaw % 100)) / 100; 
-    }
-    if(newRaw <= 0){
-      crossings += (-newRaw - (-newRaw % 100)) /100 + (prev == 0 ? 0 : 1);
-    }
-
-    numberOfZeros += crossings;
+    int newRaw = currentDialNumber + delta;
+    if(delta > 0)
+      numberOfZeros += (newRaw - (newRaw % 100)) / 100; 
+    if(newRaw <= 0)
+      numberOfZeros += (-newRaw - (-newRaw % 100)) /100 + (currentDialNumber == 0 ? 0 : 1);
     currentDialNumber = ((newRaw % 100) + 100) % 100;
 }
-
 Console.WriteLine(numberOfZeros);
