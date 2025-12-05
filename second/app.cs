a53f1b98f8fef4c5fec14f4d352d3b8d5ce75a38
dimitri <andre.d3003@gmail.com>
2025-12-05 16:03:46 +0100

    second first half

--- second/app.cs
+++ second/app.cs
@@ -2,6 +2,16 @@ using System;
 using Microsoft.VisualBasic.FileIO;
 
 string filePath = "data.csv";
+ulong total = 0;
+
+
+ulong GetNumberFromPosition(string numberAsString, int start, int? end = null){
+  int length = end.HasValue
+        ? end.Value - start
+        : numberAsString.Length - start;
+  return ulong.Parse(numberAsString.Substring(start, length));
+}
+
 using (TextFieldParser parser = new TextFieldParser(filePath))
 {
     parser.TextFieldType = FieldType.Delimited;
@@ -12,8 +22,25 @@ using (TextFieldParser parser = new TextFieldParser(filePath))
         string[] fields = parser.ReadFields();
         foreach (var field in fields)
         {
-            Console.WriteLine(field);
+          var dashPosition = field.IndexOf("-");
+          var firstNumber = GetNumberFromPosition(field, 0, dashPosition);
+          var secondNumber = GetNumberFromPosition(field, dashPosition + 1);
+
+          for(var number = firstNumber; number <= secondNumber; number++){
+            var s = number.ToString();
+
+            var halfPoint = s.Length / 2;
+            if (s.Length % 2 != 0) continue;
+
+            var firstHalf = GetNumberFromPosition(s, 0, halfPoint);
+            var secondHalf = GetNumberFromPosition(s, halfPoint);
+
+            if(firstHalf == secondHalf)
+              total += number;
+          }
         }
-        Console.WriteLine("-----");
     }
 }
+
+
+Console.WriteLine("TOTAL = " + total);
