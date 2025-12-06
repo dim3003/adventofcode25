825c6ce62fe51ef0ac9beabfa216de8dfe5dcfe6
dimitri <andre.d3003@gmail.com>
2025-12-06 14:36:14 +0100

    third part two

--- third/app.cs
+++ third/app.cs
@@ -1,13 +1,5 @@
 double total = 0;
 
-
-(int maxValue, int maxIndex) GetMaxValueAndIndex(int remainingNumberOfBatteries, Array digits, int lastMaxIndex){ 
-  var digitsTruncated = digits.Skip(maxIndexFirst + 1).Take(digits.Count() - 10).ToArray();
-  int maxValueSecond = digitsTruncated.Max();
-  int maxIndexSecond = digitsTruncated.ToList().IndexOf(maxValueSecond);
-  return (maxValue, maxIndex);
-}
-
 foreach (var line in File.ReadLines("data.txt"))
 {
   int l = line.Length;
@@ -16,18 +8,12 @@ foreach (var line in File.ReadLines("data.txt"))
     digits[i] = line[i] - '0';
   }
 
-  var digitsMinusLast = digits.Take(digits.Count() - 11).ToArray(); 
-  int maxValueFirst = digitsMinusLast.Max(); 
-  int maxIndexFirst = digitsMinusLast.ToList().IndexOf(maxValueFirst);
-  var digitsTruncated = digits.Skip(maxIndexFirst + 1).Take(digits.Count() - 10).ToArray();
-  int maxValueSecond = digitsTruncated.Max();
-  int maxIndexSecond = digitsTruncated.ToList().IndexOf(maxValueSecond);
-
+  int maxDigitIndex = -1; // First loop should not skip anything
   for(var n = 11; n >= 0; n--){
-    (int maxValue, int maxIndex) = GetMaxValueAndIndex();
-    total += maxValueFirst * Math.Pow(10, n);
+    digits = digits.Skip(maxDigitIndex + 1).ToArray(); 
+    int maxDigit = digits.Take(digits.Count() - n).Max(); 
+    maxDigitIndex = digits.ToList().IndexOf(maxDigit);
+    total += maxDigit * Math.Pow(10, n);
   }
-  Console.WriteLine(total);
-  break;
 }
 Console.WriteLine(total);
